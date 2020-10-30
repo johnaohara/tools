925801c3e64a868c064fb253f3578cfa5c8408da
John O'Hara <johara@redhat.com>
2020-10-30 20:31:13 +0000

    Add timeout clean-up usage message and cleanup before exiting

--- scripts/startup/time.js
+++ scripts/startup/time.js
@@ -2,11 +2,14 @@ const spawn = require("child_process").spawn;
 const request = require("request");
 
 const pingIntervalMs = 5;
+const timeoutMs = 10_000;
 
 const args = process.argv.slice(2);
-if (args.length == 0) {
-  console.log('The path of java executable jar must be specified !');
-  exit(1);
+
+if (args.length < 2) {
+  console.error('Missing cmd and URL!');
+  printUsage();
+  process.exit(1);
 }
 
 const procCmd = args[0];
@@ -18,13 +21,17 @@ const startTime = new Date().getTime();
 const proc = spawn(cmd[0], cmd.slice(1), { stdio: 'ignore' });
 
 const intervalHandle = setInterval(() => {
+
+  if ( new Date().getTime() - startTime > timeoutMs){
+    console.error("Response not receieved within timeout of %d ms", timeoutMs);
+    exitProcess(1);
+  };
+
   request(targetUrl, (error, response, body) => {
       if (!error && response && response.statusCode === 200 && body) {
           const time = new Date().getTime() - startTime;
           console.log(time + " ms");
-          clearInterval(intervalHandle);
-          proc.kill();
-          process.exit(0);
+          exitProcess(0);
       } else {
           // @ts-ignore
           if (!error || !error.code === "ECONNREFUSED") {
@@ -34,3 +41,17 @@ const intervalHandle = setInterval(() => {
     }
   );
 }, pingIntervalMs);
+
+
+function exitProcess(exitCode){
+  clearInterval(intervalHandle);
+  proc.kill();
+  process.exit(exitCode);
+}
+
+function printUsage(){
+  console.log('Usage: node time.js "cmd" "url"');
+  console.log('');
+  console.log('"cmd": full command to start process');
+  console.log('"url": URL that returns a HTTP 200 response when process is resonding to HTTP GET requests');
+}
